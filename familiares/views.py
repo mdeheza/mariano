dd48b57891de2c4b12bcd3760778718c4ea903d5
mariano deheza <marianodeheza70@gmail.com>
2022-06-01 23:16:09 -0300

    first commit

--- familiares/views.py
+++ familiares/views.py
@@ -6,13 +6,24 @@ from familiares.models import Familiares
 
 
 def familiares1(request):
-    nuevo_familiar = Familiares.objets.create(
+    nuevo_familiar = Familiares.objects.create(
         dni=36420295,
         nombre='Javier',
         apellido='Deheza',
         edad=31,
         hijos=11,
         activo=True)
+    
+# def familiares_add(request):
+#     new_familiares = familiares.objects.all()
+#     context = {'new_familiares':new_familiares}
+#     return render(request,'index.html',context=context)
+
+# {% for familia in new_familiares %}
+   
+#         {{familia.nombre}}<br>
+
+#       {% endfor %}
 
     context = {'nuevo_familiar': nuevo_familiar}
     return render(request, 'familiares.html', context=context)
